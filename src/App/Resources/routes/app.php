0e653244ab30452f6e55e4656270ee24cf4024f2
Vadumee <david-lebrundl@hotmail.fr>
2016-11-15 15:38:36 +0100

    ajout d'une vue (non terminée) et des routes pour l'ajout d'épreuve

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -1,3 +1,6 @@
 <?php
 
 $app->get('/', 'AppController:home')->setName('home');
+
+$app->get('/epreuve/add', 'EpreuveController:getAddEpreuve')->setName('epreuve.add');
+$app->post('/epreuve/add', 'EpreuveController:postAddEpreuve');
