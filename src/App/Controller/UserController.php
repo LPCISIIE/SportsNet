24a6ee6982cdf1b468d9711a444e203301095e92
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-15 20:26:53 +0100

    Added controller method for user profile edition

--- src/App/Controller/UserController.php
+++ src/App/Controller/UserController.php
@@ -4,9 +4,44 @@ namespace App\Controller;
 
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as V;
 
 class UserController extends Controller
 {
+    public function monCompte(Request $request, Response $response)
+    {
+        $user = $this->user();
+        $organisateur = $user->organisateur;
+
+        if ($request->isPost()) {
+            $this->validator->validate($request, [
+                'nom' => V::length(1, 50),
+                'prenom' => V::length(1, 50),
+                'paypal' => V::optional(V::email()),
+                'email' => V::email()
+            ]);
+
+            if ($this->validator->isValid()) {
+                $user->email = $request->getParam('email');
+                $user->save();
+
+                $organisateur->fill([
+                    'nom' => $request->getParam('nom'),
+                    'prenom' => $request->getParam('prenom'),
+                    'paypal' => $request->getParam('paypal')
+                ]);
+                $organisateur->save();
+
+                $this->flash('success', 'Votre compte a bien été modifié !');
+                return $this->redirect($response, 'user.compte');
+            }
+        }
+
+        return $this->view->render($response, 'User/mon-compte.twig', [
+            'organisateur' => $organisateur
+        ]);
+    }
+
     public function mesEvenements(Request $request, Response $response)
     {
         return $this->view->render($response, 'User/mes-evenements.twig', [
