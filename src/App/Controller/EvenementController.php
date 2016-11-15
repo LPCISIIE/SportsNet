402e4c834a28dc9c01a69bd3bedd8a7aaf4b7760
iryu54 <corentinlabroche@gmail.com>
2016-11-15 15:11:11 +0100

    Recuperation de l'evenement à partir de l'url

--- src/App/Controller/EvenementController.php
+++ src/App/Controller/EvenementController.php
@@ -5,13 +5,15 @@ namespace App\Controller;
 use Cartalyst\Sentinel\Checkpoints\ThrottlingException;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use App\Model\Organisateur as Organisateur;
+use App\Model\Evenement as Evenement;
 use Respect\Validation\Validator as V;
 
 class EvenementController extends Controller
 {
-    public function show(Request $request, Response $response){
-
+    public function show(Request $request, Response $response, $args){
+        $id_evenement = $args["id_evenement"];
+        $evenement = Evenement::find($id_evenement);
+        return $this->view->render($response, 'Evenement/show.twig', compact('evenement'));
     }
 
 }
