a3e267732bcf39e6c4029ce28cf0d756e90933e3
xchopin <xchopin54@gmail.com>
2016-11-15 10:35:12 +0100

    Created Eloquent Model

---
+++ src/App/Model/Evenement.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xavier
+ * Date: 15/11/2016
+ * Time: 10:24
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+class Evenement extends Model
+{
+    protected $table = 'evenement';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nom',
+        'date_debut',
+        'date_fin',
+        'adresse',
+        'telephone',
+        'discipline',
+        'etat',
+    ];
+
+
+
+    public function cloturer()
+    {
+        $this->etat = 0;
+    }
+
+    public function ouvrir()
+    {
+        $this->etat = 1;
+    }
+}
