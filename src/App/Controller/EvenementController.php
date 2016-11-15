390acbd5d158e5f87fac8c3f7a48e0c54041652d
iryu54 <corentinlabroche@gmail.com>
2016-11-15 16:35:32 +0100

    Controller: Evenement
    Envoi de l'evenement et des epreuves concernés a la vue

--- src/App/Controller/EvenementController.php
+++ src/App/Controller/EvenementController.php
@@ -6,6 +6,7 @@ use Cartalyst\Sentinel\Checkpoints\ThrottlingException;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use App\Model\Evenement as Evenement;
+use App\Model\Epreuve as Epreuve;
 use Respect\Validation\Validator as V;
 
 class EvenementController extends Controller
@@ -13,7 +14,9 @@ class EvenementController extends Controller
     public function show(Request $request, Response $response, $args){
         $id_evenement = $args["id_evenement"];
         $evenement = Evenement::find($id_evenement);
-        return $this->view->render($response, 'Evenement/show.twig', compact('evenement'));
+        $epreuves = $evenement->epreuves()->get()->toArray();
+        $evenement = $evenement->toArray();
+        return $this->view->render($response, 'Evenement/show.twig', compact('evenement', 'epreuves'));
     }
 
 }
