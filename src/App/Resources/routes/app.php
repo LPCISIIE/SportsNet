a09494ec3d3f39236a6c2089d1326574bc53caff
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-16 11:44:55 +0100

    Changed routes

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -1,7 +1,3 @@
 <?php
 
 $app->get('/home', 'AppController:home')->setName('home');
-$app->get('/evenement/{id_evenement:[0-9]+}', 'EvenementController:show')->setName("evenement.show");
-
-$app->get('/epreuve/add', 'EpreuveController:getAddEpreuve')->setName('epreuve.add');
-$app->post('/epreuve/add', 'EpreuveController:postAddEpreuve');
