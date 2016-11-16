1faf621457d946b7f4f9edbed4e58c296ad4eb98
iryu54 <corentinlabroche@gmail.com>
2016-11-16 16:00:04 +0100

    Ajout de la route de paiement

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -3,6 +3,7 @@
 $app->get('/home', 'AppController:home')->setName('home');
 $app->get('/evenement/{id_evenement:[0-9]+}', 'EvenementController:show')->setName("evenement.show");
 $app->map(['GET', 'POST'],'/epreuves/join/{id_evenement:[0-9]+}', 'EpreuveController:join')->setName("epreuve.join");
+$app->map(['GET', 'POST'],'/epreuves/payment', 'EpreuveController:payment')->setName("epreuve.payment");
 
 $app->get('/epreuve/add', 'EpreuveController:getAddEpreuve')->setName('epreuve.add');
 $app->post('/epreuve/add', 'EpreuveController:postAddEpreuve');
