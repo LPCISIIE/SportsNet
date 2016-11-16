00a2632defb1acee1e5b46bbe109c9c2b9b8bef7
Vadumee <david-lebrundl@hotmail.fr>
2016-11-16 11:23:58 +0100

    Upload d'images fonctionnel pour la création d'épreuves

--- src/App/Controller/EpreuveController.php
+++ src/App/Controller/EpreuveController.php
@@ -37,6 +37,11 @@ class EpreuveController extends Controller
         if (!($validation->isValid())) {
             return $this->getAddEpreuve($request,$response);
         }
+
+        //on créer l'image et sa destination de stockage
+        $storage = new \Upload\Storage\FileSystem(__DIR__.'/../../../public/uploads/evenements/'.$args['id_evenement'].'/epreuves');
+        $file = new \Upload\File('epreuve_pic_link', $storage);
+
         $dated = \DateTime::createFromFormat("d-m-Y H:i",$request->getParam('date_debut')." ".$request->getParam('heure_debut'));
         $datef = \DateTime::createFromFormat("d-m-Y H:i",$request->getParam('date_fin')." ".$request->getParam('heure_fin'));
         $epreuve = new Epreuve();
@@ -48,9 +53,32 @@ class EpreuveController extends Controller
         $epreuve->etat=1;
         $epreuve->description=$request->getParam('epreuve_description');
         $epreuve->prix=$request->getParam('prix');
-
         $epreuve->evenement_id=$args['id_evenement'];
         $epreuve->save();
+
+        //on donne un nom à l'image à upload
+        $new_filename = $epreuve->id;
+        $file->setName($new_filename);
+
+        // on peut accéder aux données au cas ou
+        $data = array(
+            'name'       => $file->getNameWithExtension(),
+            'extension'  => $file->getExtension(),
+            'mime'       => $file->getMimetype(),
+            'size'       => $file->getSize(),
+            'md5'        => $file->getMd5(),
+            'dimensions' => $file->getDimensions()
+        );
+
+        // on tente l'upload d'image
+        try {
+            // Success!
+            $file->upload();
+        } catch (\Exception $e) {
+            // Fail!
+            $errors = $file->getErrors();
+        }
+
 
         return $this->view->render($response, 'Evenement/edit.twig');
     }
