ff5ddbb090d0737187abbc025376e4f437124cb6
Vadumee <david-lebrundl@hotmail.fr>
2016-11-16 17:09:11 +0100

    Récupération de csv terminée (modifications de la structure à faire)

---
+++ src/App/Model/Sportif.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+class Sportif extends Model
+{
+    protected $table = 'sportif';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'birthday'
+    ];
+    public function epreuves()
+    {
+       return $this->belongsToMany('App\Model\Epreuve', "participe");
+    }
+}
--- src/App/Resources/routes/evenement.php
+++ src/App/Resources/routes/evenement.php
@@ -6,7 +6,7 @@ $app->group('', function () {
     $this->map(['GET', 'POST'], '/evenement/create', 'EvenementController:create')->setName('evenement.create');
     $this->map(['GET', 'POST'], '/evenement/{id:[0-9]+}/edit', 'EvenementController:edit')->setName('evenement.edit');
 
-    $this->post('/evenement/{id:[0-9]+}', 'EvenementController:getParticipants')->setName('evenement.participants');
     $this->get('/evenement/{id:[0-9]+}/cancel', 'EvenementController:cancel')->setName('evenement.cancel');
     $this->get('/evenement/{id:[0-9]+}/delete', 'EvenementController:delete')->setName('evenement.delete');
+    $this->get('/evenement/{id:[0-9]+}/getlist', 'EvenementController:getParticipants')->setName('evenement.participants');
 })->add(new App\Middleware\AuthMiddleware($container));
