ee5fb9ea185cb7361524ea35b9d11286da9c2641
Xavier Chopin <xchopin54@gmail.com>
2016-11-16 17:16:25 +0100

    Fixed a fatal error

--- src/App/Resources/routes/user.php
+++ src/App/Resources/routes/user.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->get('/user/{id:[0-9]+}', 'UserController:profil')->setName('user.profile');
+$this->app('/user/{id:[0-9]+}', 'UserController:profil')->setName('user.profile');
 
 $app->group('', function () {
     $this->map(['GET', 'POST'], '/profil', 'UserController:monCompte')->setName('user.compte');
