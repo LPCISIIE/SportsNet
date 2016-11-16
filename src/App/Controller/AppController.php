d969d62244c7952982e653f7370995f072c38641
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-16 16:43:21 +0100

    Show trial page

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\Epreuve;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -10,5 +11,18 @@ class AppController extends Controller
     public function home(Request $request, Response $response)
     {
         return $this->view->render($response, 'App/home.twig');
+    }
+
+    public function show(Request $request, Response $response, array $args)
+    {
+        $epreuve = Epreuve::find($args['id']);
+
+        if (!$epreuve) {
+            throw $this->notFoundException($request, $response);
+        }
+
+        return $this->view->render($response, 'Epreuve/show.twig', [
+            'epreuve' => $epreuve
+        ]);
     }
 }
