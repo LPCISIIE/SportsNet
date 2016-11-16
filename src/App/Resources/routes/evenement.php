d4317569a2cf8bf769a1dc9fc323bd7cf3648754
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-16 15:20:01 +0100

    Event canceling

--- src/App/Resources/routes/evenement.php
+++ src/App/Resources/routes/evenement.php
@@ -5,4 +5,5 @@ $app->get('/evenement/{id_evenement:[0-9]+}', 'EvenementController:show')->setNa
 $app->group('', function () {
     $this->map(['GET', 'POST'], '/evenement/create', 'EvenementController:create')->setName('evenement.create');
     $this->map(['GET', 'POST'], '/evenement/{id:[0-9]+}/edit', 'EvenementController:edit')->setName('evenement.edit');
+    $this->get('/evenement/{id:[0-9]+}/cancel', 'EvenementController:annuler')->setName('evenement.cancel');
 })->add(new App\Middleware\AuthMiddleware($container));
