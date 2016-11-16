332a1c4d7f27881c0fa628e7115111503d914555
iryu54 <corentinlabroche@gmail.com>
2016-11-16 11:40:40 +0100

    Ajout du modele Sportif

---
+++ src/App/Model/Sportif.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sportif extends Model
+{
+    protected $table = 'sportif';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'birthday'
+    ];
+}
