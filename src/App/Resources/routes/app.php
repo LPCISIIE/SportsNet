6286914e4ed6235667e9c399e862ba84597f8de4
iryu54 <corentinlabroche@gmail.com>
2016-11-16 11:41:33 +0100

    map de la route

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -2,7 +2,7 @@
 
 $app->get('/home', 'AppController:home')->setName('home');
 $app->get('/evenement/{id_evenement:[0-9]+}', 'EvenementController:show')->setName("evenement.show");
-$app->get('/epreuves/join/{id_evenement:[0-9]+}', 'EpreuveController:join')->setName("epreuve.join");
+$app->map(['GET', 'POST'],'/epreuves/join/{id_evenement:[0-9]+}', 'EpreuveController:join')->setName("epreuve.join");
 
 $app->get('/epreuve/add', 'EpreuveController:getAddEpreuve')->setName('epreuve.add');
 $app->post('/epreuve/add', 'EpreuveController:postAddEpreuve');
