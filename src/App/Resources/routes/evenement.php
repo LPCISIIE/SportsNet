fefa93e237d3ac64b690b25650a8175ba33a8bd0
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-17 11:51:08 +0100

    Added file upload when creating an event

--- src/App/Resources/routes/evenement.php
+++ src/App/Resources/routes/evenement.php
@@ -3,7 +3,7 @@
 $app->get('/evenement/{id_evenement:[0-9]+}', 'EvenementController:show')->setName('evenement.show');
 
 $app->group('', function () {
-    $this->map(['GET', 'POST'], '/evenement/create', 'EvenementController:create')->setName('evenement.create');
+    $this->map(['GET', 'POST'], '/evenement/add', 'EvenementController:add')->setName('evenement.create');
     $this->map(['GET', 'POST'], '/evenement/{id:[0-9]+}/edit', 'EvenementController:edit')->setName('evenement.edit');
 
     $this->get('/evenement/{id:[0-9]+}/cancel', 'EvenementController:cancel')->setName('evenement.cancel');
