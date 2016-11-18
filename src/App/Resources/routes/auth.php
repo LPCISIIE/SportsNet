1ba0c012849be8ad5e7e4f81a3b6bf71452be177
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-18 14:10:42 +0100

    Changed routes

--- src/App/Resources/routes/auth.php
+++ src/App/Resources/routes/auth.php
@@ -5,6 +5,4 @@ $app->group('', function () {
     $this->map(['GET', 'POST'], '/register', 'AuthController:register')->setName('register');
 })->add(new App\Middleware\GuestMiddleware($container));
 
-$app->group('', function () {
-    $this->get('/logout', 'AuthController:logout')->setName('logout');
-})->add(new App\Middleware\AuthMiddleware($container));
+$app->get('/logout', 'AuthController:logout')->setName('logout')->add(new App\Middleware\AuthMiddleware($container));
