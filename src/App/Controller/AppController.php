1e970da2725fabc584288d0a8cec220228401b68
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-18 22:22:32 +0100

    Don't display events that have just been created without being validated

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -14,7 +14,7 @@ class AppController extends Controller
             return $this->redirect($response, 'login');
         }
 
-        $evenements = Evenement::query()->orderBy('id', 'desc')->take(10)->get();
+        $evenements = Evenement::where('etat', '<>', Evenement::CREE)->orderBy('id', 'desc')->take(10)->get();
 
         return $this->view->render($response, 'App/home.twig', [
             'evenements' => $evenements
