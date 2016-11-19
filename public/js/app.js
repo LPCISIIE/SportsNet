3c3038555af29ec3891336302aad5009369ba0ad
iryu54 <corentinlabroche@gmail.com>
2016-11-19 05:08:53 +0100

    Les changements et ajout d'images font une preview

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,43 @@ $(function () {
         });
     });
 
+    function readURL(input) {
+
+
+    }
+
+    $(".modifyPicHeader input").change(function(){
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('.page-header').fadeTo('slow',0.1, function(){
+                    $(this).css('background-image', 'url(' + e.target.result + ')')
+                }).delay(500).fadeTo('slow', 1);
+                console.log(e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+
+    $(".galerryAddPic input").change(function(){
+
+        if (this.files && this.files[0]) {
+            $('.galleryPreview').empty()
+            for (var i = 0; i < this.files.length; i++) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var li=$('<li class="col-xs-6"></li>')
+                    var img=$('<img src="'+e.target.result+'"/>')
+                    li.append(img);
+                    $('.galleryPreview').append(li)
+
+                    console.log(e.target.result);
+                }
+                reader.readAsDataURL(this.files[i]);
+                this.files[i]
+            }
+
+        }
+    });
+
 });
